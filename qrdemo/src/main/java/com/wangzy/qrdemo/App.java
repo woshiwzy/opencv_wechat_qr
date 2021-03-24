396891272002be5a3c523353ba0d0d3f914a8927
WANGZHENGYUN <wangzhengyun@travelsky.com>
2021-03-24 09:04:35 +0800

    添加DemoAPK

---
+++ qrdemo/src/main/java/com/wangzy/qrdemo/App.java
@@ -0,0 +1,15 @@
+package com.wangzy.qrdemo;
+
+import android.app.Application;
+
+/**
+ * Created by wangzy on 3/16/21
+ * description:
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
